5dc54e8aab93b340dd6e3f53cd1ff972ca7a7029
Julia Cuervo <juliahcuervo@gmail.com>
2018-07-10 22:54:13 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,12 @@
 def using_until
         levitation_force = 6
         #your code here
-    
+    until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+    puts "your levitation_force is now #{levitation_force}"
+  end
+  puts "the feather is now in the air!"
 end
 
+using_until
