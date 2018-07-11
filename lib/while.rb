4f0b44d4da91490de5d27c4facff293a06b733ef
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-11 16:32:16 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -3,7 +3,9 @@ def using_while
 	while levitation_force < 10 
   	puts "Wingardium Leviosa"
   	levitation_force += 1 
+  	until
   	puts "The levitation force is now at #{levitation_force}"
+  	break
   	end
   	puts "Your feather is a flyin!".upcase
 end
